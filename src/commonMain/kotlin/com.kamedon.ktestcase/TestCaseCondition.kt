473f26caf0c2a7077916acc92306af9b5784503c
kamedon <hkamei@gmail.com>
2021-10-23 23:06:26 +0900

    refactoring

--- src/commonMain/kotlin/com.kamedon.ktestcase/TestCaseCondition.kt
+++ src/commonMain/kotlin/com.kamedon.ktestcase/TestCaseCondition.kt
@@ -3,7 +3,7 @@ package com.kamedon.ktestcase
 data class TestCaseCondition(val title: String)
 data class TestCaseConditions(val conditions: List<TestCaseCondition>)
 
-class TestCaseConditionsBuilder() {
+class TestCaseConditionsBuilder {
     private val conditions = mutableListOf<TestCaseCondition>()
 
     fun condition(title: String) {
--- src/commonMain/kotlin/com.kamedon.ktestcase/TestSuite.kt
+++ src/commonMain/kotlin/com.kamedon.ktestcase/TestSuite.kt
@@ -1,8 +1,5 @@
 package com.kamedon.ktestcase
 
-fun suite(title: String, init: TestSuiteBuilder.() -> Unit): TestSuite {
-    return TestSuiteBuilder(title).apply(init).build()
-}
 
 data class TestSuite(val title: String, val cases: List<TestCase>)
 
